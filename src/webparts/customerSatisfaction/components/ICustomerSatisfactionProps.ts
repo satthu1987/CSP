2e6873d8121c1ff388d8f9d689e71fe922176685
satthu1987 <31646312+satthu1987@users.noreply.github.com>
2026-06-29 14:07:15 +0700

    first commit

---
+++ src/webparts/customerSatisfaction/components/ICustomerSatisfactionProps.ts
@@ -0,0 +1,7 @@
+export interface ICustomerSatisfactionProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/customerSatisfaction/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ICustomerSatisfactionWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'CustomerSatisfactionWebPartStrings' {
+  const strings: ICustomerSatisfactionWebPartStrings;
+  export = strings;
+}
